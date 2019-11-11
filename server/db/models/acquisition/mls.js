099bbb274e25b99e48745531283b2e6e38f8d02d
Adam <adampoznanski@outlook.com>
2019-11-11 07:10:56 -0500

    created hot sheet BP, rough schema for mls listing.

--- server/db/models/acquisition/mls.js
+++ server/db/models/acquisition/mls.js
@@ -1,10 +1,28 @@
 //targeted mls listings
 const mlsSchema = new mongoose.Schema({
-  property:{
+  zip:{
     type: String,
     required: true
   },
-
+  county:,
+  areaName:,
+  listNum:,
+  status:,
+  listPrice:,
+  yearBuilt:,
+  address:,
+  agentID:,
+  beds:,
+  fBath:,
+  pBath:,
+  style:,
+  contact:{
+    contactName:,
+    number:,
+    office:,
+  },
+  closingDate:,
+  soldPrice:,
 
 });
 
