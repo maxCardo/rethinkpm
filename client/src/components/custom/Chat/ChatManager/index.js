e2ba55d2823af52069cc78d9b2178c8d62447744
Oscar <lagartoverde97@gmail.com>
2020-02-04 18:27:24 +0100

    removed ref error

--- client/src/components/custom/Chat/ChatManager/index.js
+++ client/src/components/custom/Chat/ChatManager/index.js
@@ -57,7 +57,7 @@ export class index extends Component {
         <ChatInitiator contacts={this.props.chats} onAddChat={this.handleAddChat}/>
         {this.props.openChats.map((chatId, index) => {
           const chat = this.props.chats.find((chat) => chat.id === chatId)
-          return <Chat key={`chat-${index}`} id={chat.id} ref={this.refsArray[index]} open={chat.open} name={chat.name} messages={chat.messages} status={chat.status} onClose={this.closeChat.bind(this,chatId)} botOn={chat.botOn}/>
+          return <Chat key={`chat-${index}`} id={chat.id}  open={chat.open} name={chat.name} messages={chat.messages} status={chat.status} onClose={this.closeChat.bind(this,chatId)} botOn={chat.botOn}/>
         })}
       </div>
     )
@@ -73,10 +73,8 @@ export class index extends Component {
       }
     })
     if(chatAlreadyAdded) {
-      this.refsArray[index].current.toggleOpen()
     } else {
       this.props.openChat(chatToAdd.id)
-      this.refsArray.push(React.createRef())
     }
   }
   closeChat(id) {
--- client/src/components/custom/Chat/common/Conversation.js
+++ client/src/components/custom/Chat/common/Conversation.js
@@ -6,7 +6,7 @@ import './common.css'
 export class Conversation extends Component {
   render() {
     return (
-      <div className='chat__conversation' ref={this.props.chatRef}>
+      <div className='chat__conversation' >
         {this.props.messages.map((message, index) => (
           <Message key={`message-${index}`} username={message.sender} message={message.content} userMessage={message.userMessage} date={message.date} />
         ))}
