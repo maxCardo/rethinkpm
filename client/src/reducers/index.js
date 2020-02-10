e3e84fdc28a7a5d6a95ad964bb840787b763c095
Oscar <lagartoverde97@gmail.com>
2020-02-10 20:43:08 +0100

    fixed profile table scrolling and added data to redux

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,5 +3,6 @@ import alert from './alert';
 import auth from './auth';
 import chat from './chat'
 import dashboard from './dashboard'
+import profile from './profile'
 
-export default combineReducers({alert, auth, chat, dashboard});
+export default combineReducers({alert, auth, chat, dashboard, profile});
