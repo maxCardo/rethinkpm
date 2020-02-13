7e5c649a8af950793eeab9dd0a3022f17b0bc9e3
Adam <adampoznanski@outlook.com>
2020-02-13 11:32:10 -0500

    set dash to landing, rec sms on backend

--- client/src/actions/chat.js
+++ client/src/actions/chat.js
@@ -1,3 +1,15 @@
+//testing socketIO connection
+
+// import io from 'socket.io-client';
+
+// const socket = io.connect('http://localhost:5000/')
+
+// socket.on('test', (data) => {
+//     console.log('socket test: ', data)
+// })
+
+//must import socket io to app.js for socket call to work on this page.
+
 export function loadChats(dispatch) {
   
 }
