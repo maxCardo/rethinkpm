9a81a932014ab5da806f4611885149fe5b101009
Oscar <lagartoverde97@gmail.com>
2020-02-26 16:33:28 +0100

    log data

--- client/src/components/custom/CrmDashboard/Tables.js
+++ client/src/components/custom/CrmDashboard/Tables.js
@@ -34,7 +34,7 @@ export class Tables extends Component {
         {
           accessor: 'status.scheduled.schDate',
           label: 'Appointment',
-          mapper: (data) => data ? new Intl.DateTimeFormat().format(new Date(data)) : 'No Appointment'
+          mapper: (data) => console.log(data) || data ? new Intl.DateTimeFormat().format(new Date(data)) : 'No Appointment'
         },
         {
           accessor: 'status.toured.tourRes',
