c34ce95250c49999925dbd42e46830ee5f1a4aa5
Oscar <lagartoverde97@gmail.com>
2020-03-19 17:52:34 +0100

    Added ticket screen ui

--- client/src/components/custom/service/ServiceDetail.js
+++ client/src/components/custom/service/ServiceDetail.js
@@ -20,6 +20,8 @@ export class ServiceDetail extends Component {
       status: 'In progress',
       nextDate: new Date(),
       vendor: 'WallsSmith',
+      issue: 'Renovate the main front wall of the house',
+      description: 'We have to renovate the front wall of the house, because it has several water damage, for this we will have to construct a new wall, with the same materials found in the rest of the construction and then paint it accordingly to the rest of the house. This is estimated to take 4 weeks, because it was constructed with an special material hard to find, and also we will face a period when usually the majority of the vendors are very busy. Once we finish with this task we will be ready for renting the unit. WallsSmith is on charge of the whole project and made an estimate budget of 10.000$. This will also increase the property value from 90.000$ to 110.000$ because it will make the unit attractive to new owners, and will allow us to increase the rent. The agent Oscar will supervise the whole operation and inform Adam when necesary',
       child: [
         {
           id: 345,
@@ -38,7 +40,7 @@ export class ServiceDetail extends Component {
       {
         route: 'ticket',
         display: 'Ticket',
-        component: <TicketScreen />,
+        component: <TicketScreen task={task} />,
       },
       {
         route: 'history',
