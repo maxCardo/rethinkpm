92be24968329ac66b7949bfb78dc53ab5536bfe4
Oscar <lagartoverde97@gmail.com>
2020-03-21 11:54:19 +0100

    material design rework

--- client/src/components/custom/service/DetailsBar.js
+++ client/src/components/custom/service/DetailsBar.js
@@ -5,17 +5,19 @@ export class DetailsBar extends Component {
     const task = this.props.task
     return (
       <div className='details-bar__container'>
-        <div className='details-bar__top'>
-          {task.parentId &&
-            <p>Parent ID: {task.parentId}</p>
-          }
-          <p>Opened: {this.formatDate(task.opened)}</p>
-        </div>
-        <div className='details-bar__bottom'>
-          <p>Task ID: {task.taskId}</p>
-          <p>Status: {task.status}</p>
-          <p>Next Date: {this.formatDate(task.nextDate)}</p>
-          <p>Vendor: {task.vendor}</p>
+        <div className='details-bar__card'>
+          <div className='details-bar__top'>
+            {task.parentId &&
+              <p>Parent ID: {task.parentId}</p>
+            }
+            <p>Opened: {this.formatDate(task.opened)}</p>
+          </div>
+          <div className='details-bar__bottom'>
+            <p>Task ID: {task.taskId}</p>
+            <p>Status: {task.status}</p>
+            <p>Next Date: {this.formatDate(task.nextDate)}</p>
+            <p>Vendor: {task.vendor}</p>
+          </div>
         </div>
       </div>
     )
