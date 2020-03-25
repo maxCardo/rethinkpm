8ce540d4371ddf2c4e0a93e0d876f56bb3f591c9
Oscar <lagartoverde97@gmail.com>
2020-03-25 20:20:53 +0100

    if note is too long show it in a modal

--- client/src/components/custom/service/detailsScreens/HistoryScreen.js
+++ client/src/components/custom/service/detailsScreens/HistoryScreen.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import Table from '../../Table'
-import { Form } from 'react-bootstrap'
+import { Form, Modal, Button } from 'react-bootstrap'
 import './screens.css'
 
 export class HistoryScreen extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      modalOpen: false,
+      noteOpened: ''
+    }
     this.headers = [
       {
         accessor: 'date',
@@ -13,9 +17,14 @@ export class HistoryScreen extends Component {
         mapper: (data) => new Intl.DateTimeFormat().format(new Date(data))
       },
       {
+        accessor: 'type',
+        label: 'Type'
+      },
+      {
         accessor: 'note',
         label: 'Note',
-        className: 'history-screen__note-column'
+        className: 'history-screen__note-column',
+        mapper: (data) => data.length > 120 ? data.substring(0, 120) + '...' : data
       },
       {
         accessor: 'user',
@@ -25,11 +34,13 @@ export class HistoryScreen extends Component {
     this.notes = [
       {
         date: new Date(),
-        note: 'First note',
+        type: 'Note',
+        note: 'This is a very long note. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
         user: 'Oscar'
       },
       {
         date: new Date(),
+        type: 'Note',
         note: 'Second note',
         user: 'Adam'
       }
@@ -37,6 +48,7 @@ export class HistoryScreen extends Component {
     this.logs = [
       {
         date: new Date(),
+        type: 'Log',
         note: 'This is a log',
         user: 'Oscar'
       }
@@ -49,6 +61,7 @@ export class HistoryScreen extends Component {
       },
       dataToShow: 'all'
     }
+    this.handleModalClose = this.handleModalClose.bind(this)
   }
   render() {
     return (
@@ -66,9 +79,26 @@ export class HistoryScreen extends Component {
           pageSize={5} 
           sorting={true} 
           fontSize={12}
+          onClickRow={(row) => this.setState({modalOpen: true, noteOpened: row.note})}
         />
+        <Modal size='lg' show={this.state.modalOpen} onHide={this.handleModalClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Note</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {this.state.noteOpened}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleModalClose}>
+              Close
+            </Button>
+          </Modal.Footer> 
+        </Modal>
       </div>
     )
+  }
+  handleModalClose() {
+    this.setState({modalOpen: false, noteOpened: ''})
   }
   handleChange(property, e) {
     this.setState({[property]: e.target.value})
