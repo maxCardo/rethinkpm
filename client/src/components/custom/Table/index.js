4e053cd209ca652471a018d6652d0f36f2d10374
Oscar <lagartoverde97@gmail.com>
2020-03-26 18:23:38 +0100

    Added color on hover also to the odd rows

--- client/src/components/custom/Table/index.js
+++ client/src/components/custom/Table/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Header from './Header'
 import Pagination from './Pagination';
+import './style.css'
 
 export class Table extends Component {
   constructor(props) {
