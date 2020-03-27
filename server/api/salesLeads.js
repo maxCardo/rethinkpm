7f53ebeaf9c659d4af6161a53a24f9de26b12b84
Oscar <lagartoverde97@gmail.com>
2020-03-27 20:07:02 +0100

    set endpoint of idx lead to console.log

--- server/api/salesLeads.js
+++ server/api/salesLeads.js
@@ -29,5 +29,32 @@ router.post('/web_lead', (req,res) => {
     res.status(200).send('ok')
 })
 
+//---------------------------------------------------------- New task from user form ----------------------------------------------------------//
+// @route: Post /api/sales/idx_lead;
+// @desc: create new task from user form
+// @ access: Public  *ToDo make private once tenants begin to log on on the software.
+router.post('/idx_lead', (req,res) => {
+  console.log(req.body);
+  // const { name, email, phone, subject, message } = req.body
+
+  //set to adampoznanski@outlook.com for prod
+  // const emailTo = 'adampoznanski@outlook.com'
+
+  // const emailBody = `
+  //   You received a new contact message with the following information:</br>
+  //   Name: ${name} </br>
+  //   Email: ${email} </br>
+  //   Phone: ${phone} </br>
+  //   Subject: ${subject} </br>
+  //   Message: ${message} </br>
+  // `
+
+  // const emailSubject = `New contact message about ${subject}`
+  
+  // sendEmail(emailTo, emailSubject,'', emailBody)
+  res.status(200).send('ok')
+})
+
+
 
 module.exports = router;
