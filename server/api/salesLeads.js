f542b4d1b608b67c120358bcd8546d82050e8e20
Oscar <lagartoverde97@gmail.com>
2020-03-27 20:30:56 +0100

    log mail body and mail subject

--- server/api/salesLeads.js
+++ server/api/salesLeads.js
@@ -35,21 +35,26 @@ router.post('/web_lead', (req,res) => {
 // @ access: Public  *ToDo make private once tenants begin to log on on the software.
 router.post('/idx_lead', (req,res) => {
   console.log(req.body);
-  // const { name, email, phone, subject, message } = req.body
+  const { name, lastName, email, phone, requestInfo, message, listingId } = req.body
 
   //set to adampoznanski@outlook.com for prod
-  // const emailTo = 'adampoznanski@outlook.com'
+  const emailTo = 'adampoznanski@outlook.com'
 
-  // const emailBody = `
-  //   You received a new contact message with the following information:</br>
-  //   Name: ${name} </br>
-  //   Email: ${email} </br>
-  //   Phone: ${phone} </br>
-  //   Subject: ${subject} </br>
-  //   Message: ${message} </br>
-  // `
+  const emailBody = `
+    You received a new contact message with the following information:</br>
+    Name: ${name} </br>
+    Last Name: ${lastName} </br>
+    Email: ${email} </br>
+    Phone: ${phone} </br>
+    Request Info: ${requestInfo} </br>
+    Listing ID: ${listingId} </br>
+    ${message && `Message: ${message} </br>`}
+  `
 
-  // const emailSubject = `New contact message about ${subject}`
+  const emailSubject = `New contact message requesting info about listing with id: ${listingId}`
+
+  console.log(emailSubject)
+  console.log(emailBody)
   
   // sendEmail(emailTo, emailSubject,'', emailBody)
   res.status(200).send('ok')
