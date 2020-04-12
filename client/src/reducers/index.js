df96ec3f15abc44ead8eb3aabba6dd55a50f31d5
Oscar <lagartoverde97@gmail.com>
2020-04-12 18:39:23 +0200

    remove profile reducer as it's no longer used

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,8 +3,7 @@ import alert from './alert';
 import auth from './auth';
 import chat from './chat'
 import dashboard from './dashboard'
-import profile from './profile'
 import services from './services'
 import brokerDashboard from "./brokerDashboard";
 
-export default combineReducers({alert, auth, chat, dashboard, profile, services, brokerDashboard});
+export default combineReducers({alert, auth, chat, dashboard, services, brokerDashboard});
