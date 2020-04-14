ecde6c786f38e3339e092fa2617e4a7f2d5a5cac
Oscar <lagartoverde97@gmail.com>
2020-04-15 00:35:30 +0200

    Added info about the sales to the agent profile

--- server/api/profile.js
+++ server/api/profile.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const RentLeadInq = require('../db/models/prospects/RentLeads/RentLeadInq');
 const Agent = require('../db/models/sales/agent')
+const singleFamilySalesModel = require('../db/models/sales/singleFamilySales')
+const multiSalesModel = require('../db/models/sales/multiSales')
 
 const router = express.Router();
 
@@ -24,9 +26,18 @@ router.get('/inquiry/:id', async (req, res) => {
 // @ access: Public * ToDo: update to make private
 router.get('/agent/:id', async (req, res) => {
   try {
-      const lead = await  Agent.findById(req.params.id).populate('prospect notes');
-      const notesPopulated = await  Note.populate(lead.notes, {path: 'user'})
-      lead.notes = notesPopulated
+      const leadResult = await  Agent.findById(req.params.id).populate('prospect notes');
+      const notesPopulated = await  Note.populate(leadResult.notes, {path: 'user'})
+      leadResult.notes = notesPopulated
+      const lead = Object.assign({}, leadResult._doc)
+      const agentSellsPromise = singleFamilySalesModel.find({agentId: lead.agentId})
+      const agentBuysPromise = singleFamilySalesModel.find({sellingAgentId: lead.agentId})
+      const agentMultiSalesPromise = multiSalesModel.find({agentId: lead.agentId})
+      console.log(await Promise.all([agentSellsPromise, agentBuysPromise, agentMultiSalesPromise]))
+      const [agentSellsResult, agentBuysResult, agentMultiSalesResult] = await Promise.all([agentSellsPromise, agentBuysPromise, agentMultiSalesPromise])
+      lead.agentSells = agentSellsResult
+      lead.agentBuys = agentBuysResult
+      lead.agentMultiSales = agentMultiSalesResult
       res.status(200).send(lead);
   } catch (error) {
       console.error(error);
