cfcd9c2893c9918c8b0d777cea84356baf41718f
Bozhidar Siljanoski <bozidar.siljanoski@live.com>
2020-04-23 18:31:20 +0200

    work on agent modal

--- client/src/components/custom/profile/ProfileInfo.js
+++ client/src/components/custom/profile/ProfileInfo.js
@@ -5,6 +5,8 @@ import {getData} from "../../../util/commonFunctions";
 import commonFormatters from "../../../util/commonDataFormatters";
 import Select from "react-select";
 import {agentStatus} from "../../../util/statusSchemas";
+import {Button, Modal} from "react-bootstrap";
+import Form from "react-bootstrap/Form";
 
 
 export class ProfileInfo extends Component {
@@ -19,9 +21,19 @@ export class ProfileInfo extends Component {
       },
       UI: {
         statusEditable: true
+      },
+      profileOpened: props.inquiry,
+      modalData: {
+
       }
     };
-    this.makeEditable=this.makeEditable.bind(this)
+    this.makeEditable = this.makeEditable.bind(this)
+    this.handleEditProfileClose = this.handleEditProfileClose.bind(this)
+    this.handleEditProfile = this.handleEditProfile.bind(this)
+    this.handleStatusChange = this.handleStatusChange.bind(this)
+    this.handleModalInputChange = this.handleModalInputChange.bind(this)
+    this.handleModalSaveData = this.handleModalSaveData.bind(this)
+
 
   }
 
@@ -59,12 +71,30 @@ export class ProfileInfo extends Component {
     });
   }
 
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.inquiry !== this.props.inquiry) {
+      this.setState({
+        statusSelected: {
+          label: this.props.inquiry.status[0] ? (this.props.inquiry.status[0].toUpperCase() + this.props.inquiry.status.slice(1)) : '',
+          value: this.props.inquiry.status ? this.props.inquiry.status : ''
+        },
+        UI: {
+          statusEditable: {
+            label: this.props.inquiry.status[0] ? (this.props.inquiry.status[0].toUpperCase() + this.props.inquiry.status.slice(1)) : '',
+            value: this.props.inquiry.status ? this.props.inquiry.status : ''
+          }
+        }
+      });
+    }
+  }
+
+
   render() {
 
     let col1 = [];
     let col2 = [];
     let col3 = [];
-    this.props.attributes.map((attribute, idx) => {
+    this.props.attributes.forEach((attribute, idx) => {
 
       if (attribute.col === 1) {
         col1.push(
@@ -72,13 +102,12 @@ export class ProfileInfo extends Component {
             (<div key={idx} dangerouslySetInnerHTML={{
               __html: (
                 `<p>
-                 <b>${attribute.name}</b>: ${this.formatData(attribute.formatter, getData(this.props.inquiry, attribute))}
+                 <b>${attribute.name}:</b> ${this.formatData(attribute.formatter, getData(this.props.inquiry, attribute))}
                </p>`
               )
             }}/>) :
             (<p key={idx}>
-              <b>{attribute.name}</b>: {getData(this.props.inquiry, attribute) ? getData(this.props.inquiry, attribute) : 'Not Implemented'}
-              {attribute.editable ? 'shet' : ''}
+              <b>{attribute.name}:</b> {getData(this.props.inquiry, attribute) ? getData(this.props.inquiry, attribute) : 'Not Implemented'}
             </p>)
         );
       } else if (attribute.col === 2) {
@@ -87,13 +116,12 @@ export class ProfileInfo extends Component {
             (<div key={idx} dangerouslySetInnerHTML={{
               __html: (
                 `<p>
-                 <b>${attribute.name}</b>: ${this.formatData(attribute.formatter, getData(this.props.inquiry, attribute))}
+                 <b>${attribute.name}:</b> ${this.formatData(attribute.formatter, getData(this.props.inquiry, attribute))}
                </p>`
               )
             }}/>) :
             (<p key={idx}>
-              <b>{attribute.name}</b>: {getData(this.props.inquiry, attribute) ? getData(this.props.inquiry, attribute) : 'Not Implemented'}
-              {attribute.editable ? 'shet' : ''}
+              <b>{attribute.name}:</b> {getData(this.props.inquiry, attribute) ? getData(this.props.inquiry, attribute) : 'Not Implemented'}
             </p>)
         );
       } else if (attribute.col === 3) {
@@ -114,14 +142,14 @@ export class ProfileInfo extends Component {
                   <Fragment>
                     <Select
                       className={`editable-${!this.state.UI.statusEditable} agentStatusEdit`}
-                      onChange={value => console.log(value)}
-                      defaultValue={this.state.statusSelected}
+                      value={this.state.statusSelected}
                       options={this.state.agentStatusSelect}
                       placeholder='Select Status'
                       isDisabled={this.state.UI.statusEditable}
+                      onChange={this.handleStatusChange}
                     />
-                    <button className='action-buttons__button singleFieldEdit'  onClick={this.makeEditable}>
-                      <i className="fas fa-edit"></i>
+                    <button className='action-buttons__button singleFieldEdit' onClick={this.makeEditable}>
+                      <i className="fas fa-pencil-alt"></i>
                     </button>
                   </Fragment>
                 ) : '')
@@ -136,9 +164,7 @@ export class ProfileInfo extends Component {
       <div className='profile-info__main-container'>
         <div className='profile-info__icon-container'>
           <ProfileIcon name={'Tests'} size={80}/>
-          <button className='action-buttons__button edit-profile__button'>
-            <i className="fas fa-cogs"></i>
-          </button>
+
         </div>
         <div className='profile-info__data-container'>
           {col1.length > 0 ? (<div className="column">
@@ -177,11 +203,99 @@ export class ProfileInfo extends Component {
           <a className='action-buttons__button' href={`mailto:${this.props.email}`}>
             <i className="fas fa-envelope"></i>
           </a>
+          {this.props.isAgent ?
+            <button className='action-buttons__button edit-profile__button' onClick={this.handleEditProfile}>
+              <i className="fas fa-cogs"></i>
+            </button> : ''}
 
         </div>
+        <Modal size='lg' show={this.state.showEditProfileModal} onHide={this.handleEditProfileClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Edit Lead</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+
+            {this.props.inquiry ? (
+              <Form>
+
+                {this.props.attributes.map((attribute, idx) => {
+                  if (attribute.editable === 'input') {
+                    return (<Form.Group>
+                      <Form.Label>{attribute.name}:</Form.Label>
+                      <Form.Control type="text" name={attribute.accessor} value={this.state.modalData[`${attribute.accessor}`]} onChange={this.handleModalInputChange}/>
+                    </Form.Group>);
+                  } else if (attribute.editable === 'select') {
+                    return (
+                      <Form.Group>
+                        <Form.Label>{attribute.name}:</Form.Label>
+                        <Form.Control as="select" value={this.state.statusSelected.value}
+                                      onChange={this.handleModalStatusChange.bind(this)}>
+                          {this.state.agentStatusSelect.map((item) => {
+                            return <option value={item.value}>{item.label}</option>
+                          })}
+                        </Form.Control>
+                      </Form.Group>)
+                  }
+
+                })}
+              </Form>
+            ) : 'nodata'}
+          </Modal.Body>
+          <Modal.Footer className="modalFooterBtns">
+            <Button className="btn btn-success" variant="secondary" onClick={this.handleModalSaveData}>
+              <i className="fas fa-pencil-alt"></i> Save
+            </Button>
+            <Button className="btn btn-danger" variant="secondary" onClick={this.handleEditProfileClose}>
+              Cancel
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     )
   }
+
+  handleModalInputChange(event) {
+    console.log(event.target.name);
+    this.setState({
+      modalData: {
+        ...this.state.modalData,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+
+
+  handleEditProfileClose() {
+    this.setState({showEditProfileModal: false})
+  }
+
+  handleEditProfile() {
+    this.setState({
+      showEditProfileModal: true,
+      modalData: {
+        firstName: this.props.inquiry.firstName,
+        lastName: this.props.inquiry.lastName,
+        phone: this.props.inquiry.phone,
+        emailAddress: this.props.inquiry.emailAddress,
+        status: this.props.inquiry.status
+      }
+    })
+  }
+
+  handleModalSaveData() {
+    console.log(this.state.modalData);
+  }
+
+  handleModalStatusChange(event) {
+    const newValue = {label: (event.target.value[0] + event.target.value.slice(1)), value: event.target.value};
+    this.setState({statusSelected: newValue});
+
+  }
+
+  handleStatusChange = selectedOption => {
+    this.setState({statusSelected: selectedOption});
+    console.log(`Option selected:`, selectedOption);
+  };
 }
 
 const mapStateToProps = state => ({
--- client/src/util/commonDataFormatters.js
+++ client/src/util/commonDataFormatters.js
@@ -1,28 +1,38 @@
 function formatPhone(data) {
-  if(!data) return '';
+  if (!data) return '';
   const phoneNumber = (data + '').replace(/ /g, '')
-  if(phoneNumber.length > 10) {
-    return `(${phoneNumber.substring(1,4)}) ${phoneNumber.substring(4,7)}-${phoneNumber.substring(7)}`
+  if (phoneNumber.length > 10) {
+    return `(${phoneNumber.substring(1, 4)}) ${phoneNumber.substring(4, 7)}-${phoneNumber.substring(7)}`
   } else {
-    return `(${phoneNumber.substring(0,3)}) ${phoneNumber.substring(3,6)}-${phoneNumber.substring(6)}`
+    return `(${phoneNumber.substring(0, 3)}) ${phoneNumber.substring(3, 6)}-${phoneNumber.substring(6)}`
   }
 }
 
 function formatMoney(data) {
-  if(!data) return '';
+  if (!data) return '';
   return (new Intl.NumberFormat('en-US',
     {style: 'currency', currency: 'USD'}
   ).format(data))
 }
 
+function formatLabels(data) {
+  if (!data) return '';
+  let dataString = '';
+  data.map((item) => {
+    dataString += `<span class='label__blue'>${item.name}</span>`;
+  });
+  return dataString;
+}
+
 const formatters = {
   formatPhone,
-  formatMoney
+  formatMoney,
+  formatLabels
 };
 
 
 export default function commonFormatter(name) {
-  if(formatters[name]) {
+  if (formatters[name]) {
     return formatters[name]
   }
   console.error({err: 'Formatter not found'});
