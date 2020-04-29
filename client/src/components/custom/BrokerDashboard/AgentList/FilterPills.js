8e41ae54fe31de5d3fd4e5e84b5527bac4b21b8c
Oscar <lagartoverde97@gmail.com>
2020-04-29 19:14:22 +0200

    added collapsible pill and filter sections

--- client/src/components/custom/BrokerDashboard/AgentList/FilterPills.js
+++ client/src/components/custom/BrokerDashboard/AgentList/FilterPills.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
+import Pill from './Pill'
 
 export class FilterPills extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPills: true
+    }
+  }
   render() {
     return (
       <div className='filter-pills__container'>
-        {this.props.filters.map((filter) => (
-            <div className='filter-pills__pill'>
-              {filter.field} {filter.filterType} {this.representValue(filter.value)}
-            </div>
-        ))}
+        <div className='filter-pills__title-container'>
+          <p>Filters:</p> <button onClick={() => this.setState((prevState) => ({showPills: !prevState.showPills}))}>{this.state.showPills ? 'Hide' : 'Show'}</button>
+        </div>
+        {this.state.showPills ? 
+          <div className='filter-pills__pills-container'>
+            {this.props.filters.map((filter) => (
+                <Pill text={`${filter.field} ${filter.filterType} ${this.representValue(filter.value)}`} />
+            ))}
+          </div>
+          :
+          ''
+        }
       </div>
     )
   }
