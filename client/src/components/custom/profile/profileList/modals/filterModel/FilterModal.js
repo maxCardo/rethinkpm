9c768eebbb6a1f1bf3664791ac5689e887d7a5b5
Bozhidar Siljanoski <bozidar.siljanoski@live.com>
2020-05-22 19:35:29 +0200

    Filter Modal working with field checked and empty Select

--- client/src/components/custom/profile/profileList/modals/filterModel/FilterModal.js
+++ client/src/components/custom/profile/profileList/modals/filterModel/FilterModal.js
@@ -1,18 +1,28 @@
 import React, {Fragment, useState, useEffect} from 'react'
 import {connect} from 'react-redux'
 import {Modal, Form, Button} from 'react-bootstrap';
-import {submitFilterModal as onSubmit, getFilterOptions} from '../../../../../../actions/profile' 
+import {submitFilterModal, getFilterOptions} from '../../../../../../actions/profile'
 import FilterFields from './filterFields/FilterFields'
 import Loading from '../../../../../core/LoadingScreen/Loading';
 
 
-const FilterModal = ({show, handleClose, settings:{filterFields, profileType},getFilterOptions,onSubmit, loading}) => {
+const FilterModal = ({show, handleClose, settings:{filterFields, profileType},getFilterOptions, submitFilterModal, loading}) => {
 
     const [state, setState] = useState(filterFields);
     useEffect(() => {
       getFilterOptions(profileType)
     });
     const onChange = (property,value ) => setState({ ...state, [property]: value})
+
+    const onSubmit = (data, type) => {
+        Object.keys(data).forEach((item) => {
+            if(`${data[item]['type']['value']}` === "in" && data[item]['value'].length === 0) {
+                data[item]['type'] = {label: "Don't filter", value: 'noFilter'};
+            }
+
+        });
+        submitFilterModal(data, type);
+    };
 
     return (
       <Modal size='xl' show={show} onHide={()=> {handleClose(); setState(filterFields)}}>
@@ -42,4 +52,4 @@ const mapStateToProps = state => ({
   loading: state.profile.filterOptions.loading
 });
 
-export default connect(mapStateToProps, {getFilterOptions, onSubmit})(FilterModal)
+export default connect(mapStateToProps, {getFilterOptions, submitFilterModal})(FilterModal)
