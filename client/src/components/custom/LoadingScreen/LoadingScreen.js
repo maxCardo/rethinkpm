23542b7218654303bdc0c747b123a6e98e206a78
Oscar Rodriguez <oscar@verify.as>
2020-06-20 13:58:35 +0200

    Added loading screen text that changes every 5 seconds

--- client/src/components/custom/LoadingScreen/LoadingScreen.js
+++ client/src/components/custom/LoadingScreen/LoadingScreen.js
@@ -3,8 +3,26 @@ import BeatLoader from "react-spinners/BeatLoader";
 import './style.css'
 
 export class LoadingScreen extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      phrase: '',
+      phraseIndex: 0
+    }
+  }
   render() {
+    const phrases = [
+      'We are loading a lot of data',
+      'Your data is almost ready',
+      'Just a few moments more'
+    ]
     if(this.props.loading) {
+      if(!this.interval) {
+        this.interval = setInterval(() => {
+          console.log(this.state)
+          this.setState((prevState) => ({phrase: phrases[prevState.phraseIndex] ? phrases[prevState.phraseIndex] : prevState.phrase, phraseIndex: prevState.phraseIndex+1}))
+        }, 5000)
+      }
       return (
         <div className='loading-screen__container'>
           <BeatLoader 
@@ -12,9 +30,13 @@ export class LoadingScreen extends Component {
             color={"#4285F4"}
             loading={true}
           />
+          <p>{this.state.phrase}</p>
         </div>
       )
     } else {
+      if(this.interval) {
+        clearInterval(this.interval)
+      }
       return this.props.children
     }
   }
