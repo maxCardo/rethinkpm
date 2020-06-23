b9e6abc9d50f07b53ce7fd47bf73c861bef4da2e
bozidar <bozidar.siljanoski@live.com>
2020-06-23 07:41:56 +0200

    Price Decrease mail scraped

--- server/3ps/scrape_parse/listing.js
+++ server/3ps/scrape_parse/listing.js
@@ -22,9 +22,83 @@ const  mlsPriceDec = async (html) => {
     console.log('mlsPrice');
     const data = fs.readFileSync('./server/3ps/scrape_parse/sampleScrapeFiles/mlsPriceDec.txt')
     const $ = cheerio.load(data)
-    const test = $("#test").text();
-    console.log('testing: ', test);
+    const setLength = $("#x_x_wrapperTable").length;
 
+
+
+    /*IDS*/
+    const mlsIds = $("#x_x_wrapperTable table table tbody > tr:nth-child(3) a").text();
+    let idArr = mlsIds.match(/.{1,7}/g);
+
+    /*PRICES*/
+    let mlsPrices = $("#x_x_wrapperTable table table tbody > tr:nth-child(3) > td:last-child span").text().split("$");
+        mlsPrices.shift();
+
+    /*LOCATION DATA*/
+    let mlsLoc =   $('#x_x_wrapperTable table tbody > tr:nth-child(2) > td:nth-child(2) tbody > tr:nth-child(2) span').map(function(){
+
+        return $(this).text().replace(/ +(?= )/g,'');
+    }).get();  //$("#x_x_wrapperTable table tbody > tr:nth-child(2) > td:nth-child(2) tbody > tr:nth-child(2) span").text();
+
+    /*PROPERTY TYPE*/
+    let mlsTypes = $("#x_x_wrapperTable table table tbody > tr:nth-child(4) > td:last-child span").map(function(){
+        return $(this).text();
+    }).get();
+
+    /*PROPERTY # of BEDROOMS*/
+    let mlsBedrooms = $("#x_x_wrapperTable table table tbody > tr:nth-child(5) > td:nth-child(2) span").map(function(){
+        return $(this).text();
+    }).get();
+
+    /*LISTING STYLE*/
+    let mlsStyle = $("#x_x_wrapperTable table table tbody > tr:nth-child(6) > td:nth-child(2) span").map(function(){
+        return $(this).text();
+    }).get();
+
+    /*PARKING SPACE*/
+    let mlsParking = $("#x_x_wrapperTable table table tbody > tr:nth-child(7) > td:nth-child(2)").map(function(){
+        let number = $(this).find('span:first-child').text();
+        let type = $(this).find('span:last-child').text();
+
+        return number + " / " + type;
+    }).get();
+
+
+    /*Build entry array*/
+    let entries = [];
+    for (i=0;i<setLength;i++) {
+        let entry = {};
+
+        entry.id = idArr[i];
+
+        entry.prce = mlsPrices[i];
+
+        let locationArray = mlsLoc[i].split(",");
+        entry.address = locationArray[0];
+        let entryZip = '';
+        if (locationArray.length > 1 && locationArray[1]) {
+            entryZip = locationArray[1].substr(locationArray[1].length - 5);
+        }
+        entry.zip = entryZip;
+
+        entry.type = mlsTypes[i];
+
+        entry.bedrooms = mlsBedrooms[i];
+
+        entry.style = mlsStyle[i];
+
+        entry.parking = mlsParking[i];
+
+        entries.push(entry);
+    }
+
+    console.log('testing: ', entries);
+
+    // let item = {};
+    //
+    // test.each((value, index) => {
+    //     item.address =
+    // })
     //get mls id, new price, 
     // find record in db update price
   
