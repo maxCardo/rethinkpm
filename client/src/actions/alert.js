6e6a66c47f5787bf9edb43e23bfeaab278a92de5
Oscar Rodriguez <oscar@verify.as>
2020-07-02 19:36:23 +0200

    update auth to use the new updateAlert component

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,12 +1,14 @@
 import uuid from 'uuid';
-import { SET_ALERT, REMOVE_ALERT } from './type';
+import { ALERT_FAILURE, REMOVE_ALERT } from './type';
 
-export const setAlert = (msg, alertType, timeOut = 5000) => dispatch => {
+export const setAlert = (msg, location) => dispatch => {
     const id = uuid.v4();
     dispatch({
-        type: SET_ALERT,
-        payload: {msg, alertType, id}
+      type: ALERT_FAILURE,
+      payload: {
+          heading: "Server Error",
+          msg: msg,
+          location: location
+      }
     });
-
-    setTimeout(() => dispatch({type:REMOVE_ALERT, payload: id}), timeOut);
 }
