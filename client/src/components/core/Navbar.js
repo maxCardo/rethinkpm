5f333e91fa033e2c7e44c6b3d01bbb532d749c42
bozidar <bozidar.siljanoski@live.com>
2020-07-13 17:33:47 +0200

    nav reworked with icons and popper

--- client/src/components/core/Navbar.js
+++ client/src/components/core/Navbar.js
@@ -1,28 +1,67 @@
-import React , {useEffect} from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { logout } from '../../actions/auth'
+import React, {useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {logout} from '../../actions/auth'
 import PropTypes from 'prop-types'
 import {loadUser} from '../../actions/auth';
-import {getCookie} from '../../util/cookies'
+import {getCookie} from '../../util/cookies';
+import Tooltip from './Tooltip';
 
-const Navbar = ({auth:{isAuthenticated, loginInProgress}, logout}) => {
+const Navbar = ({auth: {isAuthenticated, loginInProgress}, logout}) => {
     useEffect(() => {
-        loadUser();    
+        loadUser();
     });
     const handleLogout = () => {
-      logout()
+        logout()
     }
     const authLinks = (
         <ul>
-            <li><Link to='/profile/agentPros'>Agents</Link></li>
-            <li><Link to='/profile/buyerPros'>Buyers</Link></li>
-            <li><Link to='/profile/rentPros'>Renters</Link></li>
-            <li><Link to='/services'>Service</Link></li>
-            <li><Link to='/chat'>Communication</Link></li>
+            <li>
+                <Tooltip placement="top" trigger="hover" tooltip="Agents">
+                    <Link to='/profile/agentPros'>
+                        <i className="fas fa-user-secret"></i>
+                    </Link>
+                </Tooltip>
+            </li>
+            <li>
+                <Tooltip placement="top" trigger="hover" tooltip="Buyers">
+
+                    <Link to='/profile/buyerPros'>
+                        <i className="fas fa-user-check"></i>
+                    </Link>
+                </Tooltip>
+            </li>
+            <li>
+                <Tooltip placement="top" trigger="hover" tooltip="Renters">
+                    <Link to='/profile/rentPros'>
+                        <i className="fas fa-user-clock"></i>
+                    </Link>
+                </Tooltip>
+            </li>
+            <li>
+                <Tooltip placement="top" trigger="hover" tooltip="Service">
+                    <Link to='/services'>
+                        <i className="fas fa-concierge-bell"></i>
+                    </Link>
+                </Tooltip>
+            </li>
+            <li>
+                <Tooltip placement="top" trigger="hover" tooltip="Communication">
+                    <Link to='/chat'>
+                        <i className="fas fa-comments"></i>
+
+                    </Link>
+                </Tooltip>
+            </li>
             {/* <li><Link to="/rentroll">Current Rentals</Link></li>
             <li><Link to="/acquisition">New Acquisition</Link></li> */}
-            <li><a onClick = {handleLogout} href='/' >Logout</a></li>
+            <li>
+                <Tooltip placement="top" trigger="hover" tooltip="Logout">
+                    <a onClick={handleLogout} href='/'>
+                        <i className="fas fa-sign-out-alt"></i>
+                    </a>
+                </Tooltip>
+            </li>
         </ul>
 
     );
@@ -38,13 +77,13 @@ const Navbar = ({auth:{isAuthenticated, loginInProgress}, logout}) => {
 
     let links = guestLinks
 
-    if(isAuthenticated || (loginInProgress && getCookie('sid'))) {
-      links = authLinks
+    if (isAuthenticated || (loginInProgress && getCookie('sid'))) {
+        links = authLinks
     }
 
-        
+
     return (
-        <nav className = 'navbar bg-dark'>
+        <nav className='navbar bg-dark'>
             <h2><Link to='/'><i className="fas fa-code"></i> ReThink PM</Link></h2>
             {links}
         </nav>
@@ -57,8 +96,8 @@ Navbar.propTypes = {
 }
 
 const mapStateToProps = state => ({
-    auth:state.auth
+    auth: state.auth
 })
 
-export default connect(mapStateToProps, {logout, loadUser} )(Navbar)
+export default connect(mapStateToProps, {logout, loadUser})(Navbar)
 
