157a831ef3520042f4fa60ce01b12fff9a8fb1ff
Oscar <lagartoverde97@gmail.com>
2020-08-13 22:37:07 +0200

    remade marketplace table to fit the description of the document

--- client/src/components/custom/Marketplace/Marketplace.js
+++ client/src/components/custom/Marketplace/Marketplace.js
@@ -11,46 +11,56 @@ export class Listings extends Component {
     }
     this.headers = [
       {
-        accessor: "listNumber",
-        label: "List Number"
-      },
-      {
-        accessor: "streetNumber",
-        label: "Street Number"
-      },
-      {
-        accessor: "streetName",
-        label: "Street Name"
-      },
-      {
-        accessor: "zipcode",
-        label: "Zipcode"
-      },
-      {
-        accessor: "city",
-        label: "City"
+        accessor: "propertyType",
+        label: "Type"
       },
       {
         accessor: "county",
-        label: "County"
+        label: "Area"
       },
       {
-        accessor: "state",
-        label: "State"
-      },
-      {
-        accessor: 'bedrooms',
-        label: 'Bedroooms',
-      },
-      {
-        accessor: 'listDate',
-        label: 'List Date',
-        mapper: 'date'
+        accessor: "streetName",
+        label: "Address"
       },
       {
         accessor: 'listPrice',
         label: 'List Price',
         mapper: 'money'
+      },
+      {
+        accessor: 'bedrooms',
+        label: 'Bedrooms'
+      },
+      {
+        accessor: 'bathsFull',
+        label: 'Full Bath'
+      },
+      {
+        accessor: 'bathsPartial',
+        label: 'Partial Bath'
+      },
+      {
+        reactComponent: true,
+        label: 'Actions',
+        render: (item) => (
+          <div>
+            <a href={`http://google.com/search?q=${item.streetName}`}>
+              <i class="fas fa-link"></i>
+            </a>
+            <a>
+              <i class="fas fa-plus"></i>
+            </a>
+            <a>
+              <i class="fas fa-check"></i>
+            </a>
+            <a>
+              <i class="fas fa-adjust"></i>
+            </a>
+            <a>
+              <i class="fas fa-times"></i>
+            </a>
+          </div>
+        )
       }
     ]
   }
