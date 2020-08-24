5940c1d29b22b02424a031da4ce2c703c212d539
Oscar <lagartoverde97@gmail.com>
2020-08-24 06:44:11 +0200

    added marketplace block

--- server/api/marketplace/_.js
+++ server/api/marketplace/_.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const auth = require('../../middleware/auth')
+const requirePermission = require('../../middleware/requirePermission')
 
 
 const router = express.Router();
+router.use(auth)
+router.use(requirePermission('ACCESS_MARKETPLACE_BLOCK'))
 
 //api routes
 router.use('/web', require('./website'))
