b7a64e7b1b907cdd133ebeee7f9468c0f329d9c0
Oscar <lagartoverde97@gmail.com>
2020-10-04 17:45:11 +0200

    working on screens over 1100px heights

--- client/src/components/core/FullScreenTable/FullScreenTable.js
+++ client/src/components/core/FullScreenTable/FullScreenTable.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
+import Table from '../Table';
 
-const FullScreenTable = () => {
+const FullScreenTable = (props) => {
+  const container = useRef(null)
+  const [pageSize, setPageSize] = useState(10)
+  useEffect(() => {
+    if(container.current) {
+      const containerHeight = container.current.clientHeight
+      console.log(containerHeight)
+      const rowNumber = Math.floor(containerHeight / 48);
+      setPageSize(rowNumber)
+    }
+  }, [container.current, container.current && container.current.clientHeight]);
+
   return (
-    <div style={{backgroundColor: 'red', flex: 1}}>
-
+    <div style={{flex: 1}} ref={container}>
+      <Table 
+        {...props}
+        pageSize={pageSize}
+      />
     </div>
   )
 }
