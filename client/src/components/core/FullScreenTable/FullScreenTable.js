79c86c0eef116bdb56fd3c795ff220c1e0a2a28b
Oscar <lagartoverde97@gmail.com>
2020-10-04 18:28:55 +0200

    Working in every screen with a little hack

--- client/src/components/core/FullScreenTable/FullScreenTable.js
+++ client/src/components/core/FullScreenTable/FullScreenTable.js
@@ -1,17 +1,24 @@
 import React, { useRef, useEffect, useState } from 'react'
 import Table from '../Table';
+import {useWindowSize} from "../../../util/commonFunctions";
 
 const FullScreenTable = (props) => {
   const container = useRef(null)
   const [pageSize, setPageSize] = useState(10)
+  const [refreshCounter, setRefreshCounter] = useState(0)
+  const screen = useWindowSize();
   useEffect(() => {
     if(container.current) {
       const containerHeight = container.current.clientHeight
-      console.log(containerHeight)
+      console.log(`screen: ${screen.height} - container: ${containerHeight}`)
       const rowNumber = Math.floor(containerHeight / 48);
       setPageSize(rowNumber)
+      if(screen.height < containerHeight) {
+        setPageSize(1)
+        setRefreshCounter(refreshCounter + 1)
+      }
     }
-  }, [container.current, container.current && container.current.clientHeight]);
+  }, [container.current, container.current && container.current.clientHeight, refreshCounter]);
 
   return (
     <div style={{flex: 1}} ref={container}>
