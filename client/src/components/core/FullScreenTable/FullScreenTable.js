7629a2e727f5c32eafdd42d7cc3ff5da6381b59c
Oscar <lagartoverde97@gmail.com>
2020-10-04 18:34:04 +0200

    Added small comment to the hack

--- client/src/components/core/FullScreenTable/FullScreenTable.js
+++ client/src/components/core/FullScreenTable/FullScreenTable.js
@@ -10,9 +10,9 @@ const FullScreenTable = (props) => {
   useEffect(() => {
     if(container.current) {
       const containerHeight = container.current.clientHeight
-      console.log(`screen: ${screen.height} - container: ${containerHeight}`)
       const rowNumber = Math.floor(containerHeight / 48);
       setPageSize(rowNumber)
+      // When the screen is too big to fit the table and does scrolling we reset the table size and recalculate the pagesize
       if(screen.height < containerHeight) {
         setPageSize(1)
         setRefreshCounter(refreshCounter + 1)
