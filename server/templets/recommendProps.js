cc712df880aa38cecb8cbd659da8a7e8b4304e36
bozidar <bozidar.siljanoski@live.com>
2020-10-21 00:28:26 +0200

    email designn fixes

--- server/templets/recommendProps.js
+++ server/templets/recommendProps.js
@@ -99,9 +99,9 @@ const emailTemplate = (properties, message) => {
                             valign="top">
                             <table width="100%" cellpadding="0" cellspacing="0" border="0">
                                 <tr>
-                                    <td style="padding-right: 15px; padding-left: 15px; padding-top:15px; padding-bottom:15px;">
+                                    <td style="padding-right: 15px; padding-left: 15px; padding-top:15px; padding-bottom:15px;max-width:200px" width="200">
                         <![endif]-->
-                        <div class="col num6"
+                        <div class="col num4"
                              style="display: table-cell; vertical-align: middle; max-width: 220px; min-width: 200px; width: 200px;">
                             <div style="width:100% !important;">
                                 <!--[if (!mso)&(!IE)]><!-->
@@ -137,7 +137,7 @@ const emailTemplate = (properties, message) => {
                             <tr>
                                 <td style="padding-right: 15px; padding-left: 0; padding-top:15px; padding-bottom:15px;">
                         <![endif]-->
-                        <div class="col num6"
+                        <div class="col num8"
                              style="display: table-cell; vertical-align: middle; max-width: 420px; min-width: 400px; width: 400px;">
                             <div style="width:100% !important;">
                                 <!--[if (!mso)&(!IE)]><!-->
@@ -444,7 +444,7 @@ const emailTemplate = (properties, message) => {
                                    <!--[if mso]>
                                     <table width="100%" cellpadding="0" cellspacing="0" border="0">
                                         <tr>
-                                            <td style="padding-right: 5px; padding-left: 5px; padding-top: 5px; padding-bottom: 5px; font-family: Arial, sans-serif">
+                                            <td style="padding-right: 5px; padding-left: 5px; padding-top: 5px; padding-bottom: 5px; font-family: Arial, sans-serif; text-align:center;" align="center">
                                     <![endif]-->
                                     <div style="color:#ffffff;font-family:Helvetica Neue, Helvetica, Arial, sans-serif;line-height:1.2;padding-top:5px;padding-right:5px;padding-bottom:5px;padding-left:5px;">
                                         <div style="line-height: 1.2; font-size: 12px; color: #ffffff; font-family: Helvetica Neue, Helvetica, Arial, sans-serif; mso-line-height-alt: 14px;">
@@ -525,66 +525,6 @@ const emailTemplate = (properties, message) => {
                 </div>
             </div>
             ${templateString}
-            <div style="background-color:transparent;overflow:hidden">
-                <div class="block-grid "
-                     style="min-width: 320px; max-width: 600px; overflow-wrap: break-word; word-wrap: break-word; word-break: break-word; Margin: 0 auto; width: 100%; background-color: #ffffff;">
-                    <div style="border-collapse: collapse;display: table;width: 100%;background-color:#ffffff;">
-                        <!--[if (mso)|(IE)]>
-                        <table width="100%" cellpadding="0" cellspacing="0" border="0"
-                               style="background-color:transparent;">
-                            <tr>
-                                <td align="center">
-                                    <table cellpadding="0" cellspacing="0" border="0" style="width:600px">
-                                        <tr class="layout-full-width" style="background-color:#ffffff"><![endif]-->
-                        <!--[if (mso)|(IE)]>
-                        <td align="center" width="600"
-                            style="background-color:#ffffff;width:600px; border-top: 0px solid transparent; border-left: 0px solid transparent; border-bottom: 0px solid transparent; border-right: 0px solid transparent;"
-                            valign="top">
-                            <table width="100%" cellpadding="0" cellspacing="0" border="0">
-                                <tr>
-                                    <td style="padding-right: 0px; padding-left: 0px; padding-top:5px; padding-bottom:5px;">
-                        <![endif]-->
-                        <div class="col num12"
-                             style="min-width: 320px; max-width: 600px; display: table-cell; vertical-align: top; width: 600px;">
-                            <div style="width:100% !important;">
-                                <!--[if (!mso)&(!IE)]><!-->
-                                <div style="border-top:0px solid transparent; border-left:0px solid transparent; border-bottom:0px solid transparent; border-right:0px solid transparent; padding-top:5px; padding-bottom:5px; padding-right: 0px; padding-left: 0px;">
-                                    <!--<![endif]-->
-                                    <table class="divider" border="0" cellpadding="0" cellspacing="0" width="100%"
-                                           style="table-layout: fixed; vertical-align: top; border-spacing: 0; border-collapse: collapse; mso-table-lspace: 0pt; mso-table-rspace: 0pt; min-width: 100%; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;"
-                                           role="presentation" valign="top">
-                                        <tbody>
-                                        <tr style="vertical-align: top;" valign="top">
-                                            <td class="divider_inner"
-                                                style="word-break: break-word; vertical-align: top; min-width: 100%; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;"
-                                                valign="top">
-                                                <table class="divider_content" border="0" cellpadding="0"
-                                                       cellspacing="0" width="100%"
-                                                       style="table-layout: fixed; vertical-align: top; border-spacing: 0; border-collapse: collapse; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-top: 0px solid transparent; height: 0px; width: 100%;"
-                                                       align="center" role="presentation" height="0" valign="top">
-                                                    <tbody>
-                                                        <tr style="vertical-align: top;" valign="top">
-                                                            <td style="word-break: break-word; vertical-align: top; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;"
-                                                                height="0" valign="top"><a href="tel:+4124445181" style="max-width: 48px;color: #0068a5;margin-left: auto; margin-right: 10px;margin-top: 10px;margin-bottom: 10px; display: block;">
-                                                                <img src="https://fifthgrant.com/images/email-templates/recommendTemplate-call-btn.png" alt="Call Adam">
-                                                            </a></td>
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
-                                            </td>
-                                        </tr>
-                                        </tbody>
-                                    </table>
-                                    <!--[if (!mso)&(!IE)]><!-->
-                                </div>
-                                <!--<![endif]-->
-                            </div>
-                        </div>
-                        <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
-                        <!--[if (mso)|(IE)]></td></tr></table></td></tr></table><![endif]-->
-                    </div>
-                </div>
-            </div>
             <div style="background-color:transparent;overflow:hidden">
                 <div class="block-grid "
                      style="min-width: 320px; max-width: 600px; overflow-wrap: break-word; word-wrap: break-word; word-break: break-word; Margin: 0 auto; width: 100%; background-color: transparent;">
