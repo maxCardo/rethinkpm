52bc006298986454da8a58af2997e98a02a84e55
maxCardo <40762268+maxCardo@users.noreply.github.com>
2021-04-14 07:53:53 -0400

    started compworkup component

---
+++ client/src/components/custom/Marketplace/comps/compWorkup.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+
+//actions
+
+// like
+// blacklist
+//Property condition A-D
+//area condition
+//parking
+//stairs
+
+const CompWorkup = (data) => {
+    useEffect(() => {
+        console.log(data)
+
+    }, [])
+
+    return (
+        <div>
+            <h1>this is comp view!</h1>
+        </div>
+    )
+
+}
+
+export default CompWorkup
