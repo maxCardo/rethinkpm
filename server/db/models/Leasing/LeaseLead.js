6ed39bec3ddf1f35c105ee82affd6758e266f189
maxCardo <adampoznanski@outlook.com>
2025-04-07 07:17:34 -0400

    added leasing models to db file

---
+++ server/db/models/Leasing/LeaseLead.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose')
+
+const leaseLeadSchema = new mongoose.Schema({
+
+    firstName: String,
+    lastName: String,
+    fullName: String, 
+    email: [
+        {
+            address: String,
+            isPrimary: Boolean
+        }
+    ],
+    phoneNumbers: [
+        {
+            number: String,
+            isPrimary: Boolean,
+            okToText: Boolean
+        }
+    ],
+    notes: [
+        {
+            type: {
+                type: String,
+            },
+            content: {
+                type: String,
+            },
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+            },
+            date: {
+                type: Date,
+                default: Date.now,
+            },
+        },
+    ],
+    status: {
+        type: String,
+        default: 'new'
+    },
+    reasonForLoss: String,
+    leadOwner: {
+        type: String, 
+        default: 'System'
+    },
+    leadSource: String,
+    createDate: {
+        type: Date,
+        default: new Date()
+    },
+    leadDate: {
+        type: Date,
+        default: new Date()
+    },
+    lastContact: Date,
+    listingAddress: String, 
+    listing: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Listing',
+        }
+    ],
+    
+})
+
+module.exports = mongoose.model('LeaseLead', leaseLeadSchema)
