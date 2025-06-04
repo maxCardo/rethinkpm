80aba2aa95df70aefe3eb28d23ec109c86d02446
Adise <adise@wyze.co.il>
2025-06-04 18:53:09 -0400

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
