4720e46b5245400a2d66d5d1a8b08f9f2ec01fe2
Adise <adise@wyze.co.il>
2025-06-12 10:23:33 -0400

    removed deprecated package 'request-promise' and used axios instead

--- server/3ps/slack.js
+++ server/3ps/slack.js
@@ -1,20 +1,22 @@
-const request = require('request-promise');
+const axios = require('axios');
 const config = require('./../config/creds');
 
 // leasing bot hook
 const hook = config.slackWebHook;
- 
 // post to slack
 
-const postSlack = (record) => {
-    const slackBody = { text: record.text }
+const postSlack = async (record) => {
+    const slackBody = { text: record.text };
 
-    request({
-        url: `https://hooks.slack.com/services/${hook}`,
-        method: 'POST',
-        body: slackBody,
-        json: true
-    })
-}
+    try {
+        await axios.post(
+            `https://hooks.slack.com/services/${hook}`,
+            slackBody,
+            { headers: { 'Content-Type': 'application/json' } }
+        );
+    } catch (error) {
+        console.error('Error posting to Slack:', error);
+    }
+};
 
 module.exports = { postSlack };
