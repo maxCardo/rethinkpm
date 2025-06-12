e19b641a24c2a13c4cac595d6bbf80cc2c347c1d
Adise <adise@wyze.co.il>
2025-06-12 18:19:05 -0400

    removed duplicate line

--- client/src/components/custom/CRM/leaseLead.js
+++ client/src/components/custom/CRM/leaseLead.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react'
-import React, {useEffect, useState} from 'react'
 import {connect} from 'react-redux'
 import IconButton from "../../core/IconButton/IconButton";
 import Table from '../../core/newTable/_Table'
