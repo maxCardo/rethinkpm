92a3300200f2d7f4efb33dc84c355a8547d9a39d
Adise <adisemamo211@gmail.com>
2025-06-17 18:07:39 -0400

    cleaned up leaseleads action

--- client/src/actions/crm/leaseLeads.js
+++ client/src/actions/crm/leaseLeads.js
@@ -1,24 +1,26 @@
 import axios from "axios";
 
-import {SET_LEASELEAD_LIST, SET_LOADING} from '../type'
-import {createSuccessAlert, createErrorAlert} from '../alert'
+import { SET_LEASELEAD_LIST, SET_LOADING } from "../type";
+// import { createSuccessAlert, createErrorAlert } from "../alert";
 
-const config = { headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' } };
+// const config = {
+//   headers: { "Content-Type": "application/json", Accept: "application/json" },
+// };
 
-
-//@desc: get all flaged props deal for showcase comp 
-export const getLeaseLeadData = () => async dispatch => {
-
-    console.log('running get showcase data')
-    try {
-        const res = await axios.get('/api/crm/leaselead')
-        console.log('res: ', res)
-        dispatch({
-            type: SET_LEASELEAD_LIST,
-            payload: res.data
-        })    
-    } catch (err) {
-        console.error(err);
+//@desc: get all flaged props deal for showcase comp
+export const getLeaseLeadData = () => async (dispatch) => {
+  console.log("running get leaselead data");
+  try {
+    const res = await axios.get("/api/crm/leaselead");
+    if (res?.data) {
+      console.log("res: ", res);
+      dispatch({
+        type: SET_LEASELEAD_LIST,
+        payload: res.data,
+      });
+      return res.data;
     }
-}
-
+  } catch (err) {
+    console.error(err);
+  }
+};
