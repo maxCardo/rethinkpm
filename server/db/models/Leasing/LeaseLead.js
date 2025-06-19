a1d851ed766d94340f90d880863b8753d24ac265
Adise <adisemamo211@gmail.com>
2025-06-19 11:22:04 -0400

    add: new field in lease lead DB schema

--- server/db/models/Leasing/LeaseLead.js
+++ server/db/models/Leasing/LeaseLead.js
@@ -1,69 +1,68 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const leaseLeadSchema = new mongoose.Schema({
-
-    firstName: String,
-    lastName: String,
-    fullName: String, 
-    email: [
-        {
-            address: String,
-            isPrimary: Boolean
-        }
-    ],
-    phoneNumbers: [
-        {
-            number: String,
-            isPrimary: Boolean,
-            okToText: Boolean
-        }
-    ],
-    notes: [
-        {
-            type: {
-                type: String,
-            },
-            content: {
-                type: String,
-            },
-            user: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'User',
-            },
-            date: {
-                type: Date,
-                default: Date.now,
-            },
-        },
-    ],
-    status: {
+  firstName: String,
+  lastName: String,
+  fullName: String,
+  email: [
+    {
+      address: String,
+      isPrimary: Boolean,
+    },
+  ],
+  phoneNumbers: [
+    {
+      number: String,
+      isPrimary: Boolean,
+      okToText: Boolean,
+    },
+  ],
+  notes: [
+    {
+      type: {
         type: String,
-        default: 'new'
-    },
-    reasonForLoss: String,
-    leadOwner: {
-        type: String, 
-        default: 'System'
-    },
-    leadSource: String,
-    createDate: {
+      },
+      content: {
+        type: String,
+      },
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      date: {
         type: Date,
-        default: new Date()
+        default: Date.now,
+      },
     },
-    leadDate: {
-        type: Date,
-        default: new Date()
+  ],
+  status: {
+    type: String,
+    default: "new",
+  },
+  reasonForLoss: String,
+  leadOwner: {
+    type: String,
+    default: "System",
+  },
+  leadSource: String,
+  leadTemperature: String,
+  createDate: {
+    type: Date,
+    default: new Date(),
+  },
+  leadDate: {
+    type: Date,
+    default: new Date(),
+  },
+  lastContact: Date,
+  nextAction: Date,
+  listingAddress: String,
+  listing: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Listing",
     },
-    lastContact: Date,
-    nextAction: Date,
-    listingAddress: String, 
-    listing: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Listing',
-        }
-    ],
-    
-})
+  ],
+});
 
-module.exports = mongoose.model('LeaseLead', leaseLeadSchema)
+module.exports = mongoose.model("LeaseLead", leaseLeadSchema);
