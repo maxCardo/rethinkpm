d5b727c497cd19178a2366deddb9d94e03accbf7
Adise <adisemamo211@gmail.com>
2025-07-06 00:21:40 -0400

    feat: add LeadNextAction component for managing next actions with date and contact options

---
+++ client/src/components/custom/CRM/leaseComponents/leadDetailsFields/LeadNextAction.js
@@ -0,0 +1,78 @@
+import CustomInput from "../../../../ui/CustomInput/CustomInput";
+import CustomReactSelect from "../../../../ui/CustomReactSelect";
+import { capitalizeFirstLetter } from "../../../../../util/commonFunctions";
+
+const LeadNextAction = ({ selectedLeadItem }) => {
+  const actionTypesValues = [
+    "contact",
+    "scheduleTour",
+    "performTour",
+    "apply",
+    "",
+  ];
+  const actionTypes = actionTypesValues.map((value) => ({
+    label: capitalizeFirstLetter(value.replace(/([A-Z])/g, " $1")),
+    value,
+  }));
+  const contactByValues = ["call", "email", "sms", ""];
+
+  const contactByOptions = contactByValues.map((value) => ({
+    label: capitalizeFirstLetter(value.replace(/([A-Z])/g, " $1")),
+    value,
+  }));
+
+  const nextAction = {
+    actionDate: new Date(),
+    actionType: "contact",
+    contactBy: "",
+  };
+
+  return (
+    <div className="lead-next-action">
+      {/* Title */}
+      <div className="lead-next-action__title text-2xl mb-3">
+        Lead Next Action
+      </div>
+      <div className="lead-next-action__content flex flex-wrap gap-2">
+        {/* Next Action Date */}
+        <CustomInput
+          inputId={"nextActionDate"}
+          label={"Next Action Date"}
+          inputStyle={{ width: "20vw" }}
+          value={new Date(nextAction.actionDate).toLocaleString()}
+          readonly={false}
+        />
+        {/* Next Action Type */}
+        <div style={{ width: "20vw" }}>
+          <CustomReactSelect
+            options={actionTypes}
+            label={"Next Action Type"}
+            // value={selectedLeadItem.status}
+            placeholder={capitalizeFirstLetter(nextAction.actionType)}
+            isDisabled={true}
+          />
+        </div>
+        {/* Next Action Contact By */}
+        <div style={{ width: "20vw" }}>
+          <CustomReactSelect
+            options={contactByOptions}
+            label={"Contact By"}
+            // value={selectedLeadItem.status}
+            placeholder={capitalizeFirstLetter(nextAction.contactBy)}
+            isDisabled={true}
+          />
+        </div>
+        {/* Last Contact */}
+        <CustomInput
+          inputId={"lastContact"}
+          label={"Last Contact"}
+          inputStyle={{ width: "20vw" }}
+          value={new Date(selectedLeadItem.createDate).toLocaleString()}
+          readonly={true}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LeadNextAction;
