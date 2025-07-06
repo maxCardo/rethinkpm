fe0c8396dc0e8e922a628529b93862adb6b5e5c3
Adise <adisemamo211@gmail.com>
2025-07-06 17:25:27 -0400

    feat: added appendIcon to CustomInput component

--- client/src/components/ui/CustomInput/CustomInput.js
+++ client/src/components/ui/CustomInput/CustomInput.js
@@ -9,21 +9,37 @@ const CustomInput = ({
   value,
   readonly = false,
   type,
+  appendIcon,
+  onAppendClick,
 }) => {
   return (
     <div className="flex flex-col">
       <label htmlFor={inputId}>{label}</label>
-      <input
-        id={inputId}
-        className="form-control my-2 custom-input"
-        style={inputStyle}
-        tabIndex={0}
-        // onChange={(e) => onChange}
-        placeholder={placeholder || label}
-        value={value || ""}
-        readOnly={readonly}
-        type={type}
-      />
+      <div className="relative flex items-center">
+        <input
+          id={inputId}
+          className={`form-control my-2 custom-input ${
+            appendIcon ? "pr-10" : ""
+          }`}
+          style={inputStyle}
+          tabIndex={0}
+          onChange={onChange}
+          placeholder={placeholder || label}
+          value={value || ""}
+          readOnly={readonly}
+          type={type}
+        />
+        {appendIcon && (
+          <div className="absolute right-2 top-1/2 transform -translate-y-1/2 flex items-center">
+            <span
+              className={`append-icon ${onAppendClick ? "cursor-pointer" : ""}`}
+              onClick={onAppendClick}
+            >
+              {appendIcon}
+            </span>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
