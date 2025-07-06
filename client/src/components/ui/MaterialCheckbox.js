fbb8e82e88d60dc6f900be27fbb985e9b5ba709f
Adise <adisemamo211@gmail.com>
2025-07-06 17:45:23 -0400

    feat: add onChange handler to MaterialCheckbox component

--- client/src/components/ui/MaterialCheckbox.js
+++ client/src/components/ui/MaterialCheckbox.js
@@ -1,10 +1,10 @@
 import { Checkbox, FormControlLabel, Grid } from "@mui/material";
 
-const MaterialCheckbox = ({ label, checked }) => {
+const MaterialCheckbox = ({ label, checked, onChange }) => {
   return (
     <Grid size={{ xs: 6, md: 12 }}>
       <FormControlLabel
-        control={<Checkbox checked={checked} />}
+        control={<Checkbox checked={checked} onChange={onChange} />}
         label={label}
       />
     </Grid>
