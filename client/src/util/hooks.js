1c41f5d4cf8889753ee45ee5e552ad16e462787d
Adise <adisemamo211@gmail.com>
2025-07-07 14:16:04 -0400

    refactor: remove unnecessary documentation comments from useDebounce hook

--- client/src/util/hooks.js
+++ client/src/util/hooks.js
@@ -1,11 +1,5 @@
 import { useState, useEffect } from "react";
 
-/**
- * Custom debounce hook that delays updating a value until after a specified delay
- * @param {any} value - The value to debounce
- * @param {number} delay - The delay in milliseconds
- * @returns {any} - The debounced value
- */
 export const useDebounce = (value, delay) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
